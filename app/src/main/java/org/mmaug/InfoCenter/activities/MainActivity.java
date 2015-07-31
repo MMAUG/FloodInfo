94123590424c34de1cabed676fd0df05d2f8179c
SH <swanhtet1992@gmail.com>
2015-08-01 04:44:31 +0630

    Dafuq is wrong with onClick xml. Replace with Butterknife

--- app/src/main/java/org/mmaug/InfoCenter/activities/MainActivity.java
+++ app/src/main/java/org/mmaug/InfoCenter/activities/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import mmaug.org.yaybay.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,8 +14,10 @@ public class MainActivity extends AppCompatActivity {
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    ButterKnife.bind(this);
   }
 
+  @OnClick(R.id.tv_donation_team)
   public void contactClick(View view) {
     Intent i = new Intent();
     i.setClass(this, ContactsActivity.class);
@@ -21,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     startActivity(i);
   }
 
+  @OnClick(R.id.tv_latest_news)
   public void newsClick(View view) {
     Intent i = new Intent();
     i.setClass(this, NewsActivity.class);
@@ -28,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     startActivity(i);
   }
 
+  @OnClick(R.id.tv_report)
   public void reportClick(View view) {
     Intent i = new Intent();
     i.setClass(this, ReportActivity.class);
@@ -35,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
     startActivity(i);
   }
 
+  @OnClick(R.id.tv_alert_level)
   public void alertLevel(View view) {
     Toast.makeText(this, "We will implement it tonight...", Toast.LENGTH_SHORT).show();
   }
