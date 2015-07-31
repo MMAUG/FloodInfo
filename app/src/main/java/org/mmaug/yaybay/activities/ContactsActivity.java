562e0be787c5f212acba2c6c869755ce401ca940
SH <swanhtet1992@gmail.com>
2015-07-31 21:01:44 +0630

    Add contact activity

---
+++ app/src/main/java/org/mmaug/yaybay/activities/ContactsActivity.java
@@ -0,0 +1,14 @@
+package org.mmaug.yaybay.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author SH (swanhtet@nexlabs.co)
+ */
+public class ContactsActivity extends Activity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+  }
+}
