67917f9d933ba6d8276a3fb5d0f6d3c0a56673c0
Poe Poe <poepoe88@gmail.com>
2015-08-01 16:12:41 +0630

    back button

--- app/src/main/java/org/mmaug/InfoCenter/activities/NewsDetailActivity.java
+++ app/src/main/java/org/mmaug/InfoCenter/activities/NewsDetailActivity.java
@@ -1,7 +1,9 @@
 package org.mmaug.InfoCenter.activities;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import mmaug.org.yaybay.R;
@@ -20,10 +22,23 @@ public class NewsDetailActivity extends AppCompatActivity {
     setContentView(R.layout.activity_news);
     ButterKnife.bind(this);
 
+    ActionBar actionBar = getSupportActionBar();
+    if (actionBar != null) actionBar.setDisplayHomeAsUpEnabled(true);
+
     if (!getIntent().getExtras().isEmpty()) {
       News n = (News) getIntent().getSerializableExtra("news");
       tvNewsTitle.setText(n.getTitle());
       tvNewsDescription.setText(n.getDescription());
     }
+  }
+
+  @Override public boolean onOptionsItemSelected(MenuItem item) {
+    int id = item.getItemId();
+    if (id == android.R.id.home) {
+      onBackPressed();
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
   }
 }
